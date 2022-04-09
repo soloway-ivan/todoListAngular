34e9f6269791320572f76c51f1d015c36b965034
Ivan <soloveii@mydigicode.com>
2022-04-09 21:21:00 +0300

    WIP

--- src/app/task/task-list-item/task-list-item.component.ts
+++ src/app/task/task-list-item/task-list-item.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
 import { TaskInterface } from '../task.interface';
 
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { TaskInterface } from '../task.interface';
+import { TaskService } from '../task.service';
+
 @Component({
   selector: 'task-list-item',
   templateUrl: './task-list-item.component.html',
@@ -9,11 +13,64 @@ import { TaskInterface } from '../task.interface';
 
 export class TaskListItemComponent {
 
+  constructor(private taskService: TaskService) { }
+
   @Input()
   task!: TaskInterface;
 
-  ngOnChanges(OnChanges: SimpleChanges) { //WIP
-    console.log(OnChanges['task'], 'changed');
+  @ViewChild('taskName')
+  taskName: ElementRef | undefined;
+
+  @ViewChild('taskDescription')
+  taskDescription: ElementRef | undefined;
+
+  editable = false;
+
+  onEditTask() {
+    this.editable = true;
+
+    if (!this.taskName || !this.taskDescription) {
+      return;
+    }
+
+    this.taskName.nativeElement.removeAttribute('readonly');
+    this.taskDescription.nativeElement.removeAttribute('readonly');
+
+    console.log(this.taskService.allTasksList);
+
   }
-  // edit WIP
-}
+
+  updateTask(name: string, description: string): TaskInterface {
+    this.editable = false;
+
+    this.addReadonly();
+
+
+    return {
+      name: name,
+      description: description,
+      comment: '',
+      done: false
+    }
+  }
+
+  addReadonly() {
+    if (!this.taskName || !this.taskDescription) {
+      return;
+    }
+    this.taskName.nativeElement.setAttribute('readonly', true);
+    this.taskDescription.nativeElement.setAttribute('readonly', true);
+  }
+
+  undoChanges(): TaskInterface {
+    console.log(this.task);
+
+    this.editable = false;
+    this.addReadonly();
+    return {
+      name: this.task.name,
+      description: this.task.description,
+      comment: '',
+      done: false
+    }
+  }
