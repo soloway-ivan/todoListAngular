d663015688abf59e9051ec474ec0db5004822e30
Soloway Ivan <u1@soloveii2-sm.digicode.ua>
2022-10-13 14:06:53 +0300

    added filters for tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { CreateTaskComponent } from './task/create-task/create-task.component';
 import { TaskListComponent } from './task/task-list/task-list.component';
 import { TaskListItemComponent } from './task/task-list-item/task-list-item.component';
 import { TaskFilterComponent } from './task/task-filter/task-filter.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
